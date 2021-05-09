deb257dd5f121f8f462774a84d0bb1bc18ff4781
KryvosheienkoY <j.kryvosheienko@gmail.com>
2021-05-09 19:23:37 +0300

    Created img card component

--- src/App.vue
+++ src/App.vue
@@ -9,28 +9,9 @@
       </v-app-bar>
       <v-container
           class="d-flex  justify-center my-10">
-        <v-card class="col-12 col-lg-4 mx-auto"
-                max-width="450">
-          <v-img
-              contain
-              class="align-self-center"
-              max-height="400"
-              :src="selectedImage"
-          ></v-img>
-<!--          <v-img>{{ message }}</v-img>-->
-          <v-card-actions class="justify-center ma-4">
-            <v-btn color="primary" outlined @click="randomizePicture">
-              Repeat
-              <v-icon
-                  right
-                  dark
-              >
-                mdi-replay
-              </v-icon>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+        <ImageCard></ImageCard>
       </v-container>
+
     </div>
     <!--  </div>-->
   </v-app>
@@ -38,33 +19,14 @@
 
 <script>
 
-const API_URL = "http://localhost:5000/quoteimgs";
-// import axios from "axios";
-
-// const context = require.context('./assets/init_img', true, /\.jpg$|.jpeg$|.png$/);
-// console.log('context', context);
-// console.log('keys', context.keys());
-// console.log('map keys', [...context.keys()].map(k => context(k)));
-// const imgPaths = [...context.keys()].map(k => context(k));
+import ImageCard from "@/components/ImageCard";
 
 export default {
   name: 'App',
-  data() {
-    return {
-      images: [],
-      // selectedImage:  require('@/assets/init_img/image.jpg'),
-      selectedImage: API_URL,
-    }
+  components: {
+    ImageCard
   },
-  methods: {
-    randomizePicture() {
-      this.selectedImage = this.makeRandomImgURL();
-    },
-    makeRandomImgURL(){
-      return `${API_URL}?_=${~~(Math.random()*100000)}`;
-    }
 
-  }
 }
 
 </script>
---
+++ src/components/ImageCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-card class="col-12 col-lg-4 mx-auto"
+            max-width="450">
+      <v-img
+          contain
+          class="align-self-center"
+          max-height="400"
+          :src="selectedImage"
+      ></v-img>
+      <!--          <v-img>{{ message }}</v-img>-->
+      <v-card-actions class="justify-center ma-4">
+        <v-btn color="primary" outlined @click="randomizePicture">
+          Repeat
+          <v-icon
+              right
+              dark
+          >
+            mdi-replay
+          </v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+const API_URL = "http://localhost:5000/quoteimgs";
+export default {
+name: "ImageCard",
+  data() {
+    return {
+      images: [],
+      // selectedImage:  require('@/assets/init_img/image.jpg'),
+      selectedImage: API_URL,
+    }
+  },
+  methods: {
+    randomizePicture() {
+      this.selectedImage = this.makeRandomImgURL();
+    },
+    makeRandomImgURL(){
+      return `${API_URL}?_=${~~(Math.random()*100000)}`;
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
