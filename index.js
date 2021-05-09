729eca507f999f1d59496f13703036da96074b71
KryvosheienkoY <j.kryvosheienko@gmail.com>
2021-05-09 19:13:07 +0300

    Fixed img cards

---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const Jimp = require('jimp');
+const fs = require('fs');
+const pathImgFolder = 'src\\assets\\init_img\\';
+const imgPathes = fs.readdirSync(pathImgFolder);
+
+const port = 5000;
+app.use('/', express.static(__dirname + ''));
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.listen(port);
+
+
+app.get('/', (req, res) => {
+    res.send(`Hi! Server is listening on port ${port}`)
+});
+app.route("/quoteimgs")
+    .get(async (req, res) => {
+        try {
+            console.log(req.body);
+            let imgPath = pathImgFolder + imgPathes[Math.floor(Math.random() * imgPathes.length)];
+            console.log("imgPathers - ", imgPathes)
+            console.log("imgPath - ", imgPath)
+            let img = await textOverlay(imgPath);
+            console.log('path', img);
+            res.sendFile(path.resolve(__dirname, img));
+        } catch (err) {
+            res.send(err);
+        }
+    })
+
+
+async function textOverlay(imgPath) {
+    let text = generateQuote();
+    console.log("q- " + text);
+
+    let path = `src/assets/result_img/${Date.now()}_quote.png`;
+    console.log(path);
+
+    const imgPromise = new Promise((resolve) => {
+
+        Jimp.read(imgPath, (err, image) => {
+            if (err) throw err;
+            Jimp.loadFont(Jimp.FONT_SANS_32_WHITE, (err, font) => {
+                // let w = image.bitmap.width;
+                // let h = image.bitmap.height;
+                // let w = 600;
+                let h = 400;
+                image.blur(15);
+                image.resize(Jimp.AUTO, h);
+                let w = image.bitmap.width;
+                let textWidth = Jimp.measureText(font, text);
+                console.log('w -', w);
+                console.log('textWidth -', textWidth);
+                if (textWidth >= w) {
+                    image.resize(textWidth + 30, h);
+                    w = image.bitmap.width;
+                    console.log('w -', w);
+                    console.log('textWidth -', textWidth);
+                }
+                let textHight = Jimp.measureTextHeight(font, text);
+                image
+                    .print(font, w / 2 - textWidth / 2, h / 2 - textHight / 2,
+                        {
+                            text: text,
+                        }, textWidth, textHight)
+                    .write(path, () => {
+                        resolve(path);
+                    }); // save
+            });
+        })
+    });
+    return imgPromise;
+}
+
+
+let nouns = ["pain", "cake", "work", "chocolate", "success", "failure", "happiness", "runner", "rain", "sheep", "dream", "life", "sadness", "money"];
+let verbs = ["follows", "inspires", "causes", "believes", "goes", "runs", "enjoys", "creates"];
+let adjectives = ["rich", "friendly", "happy", "bright", "great", "miserable", "lovely", "tiny", "small"];
+let determiners = ["your", "my", "the", "his", "our", "many", "some"];
+
+
+// eslint-disable-next-line no-unused-vars
+function generateQuote() {
+    return capitalizeFirstLetter(determiners[Math.floor(Math.random() * determiners.length)]) + ' '
+        + adjectives[Math.floor(Math.random() * adjectives.length)] + ' ' +
+        nouns[Math.floor(Math.random() * nouns.length)] + ' ' +
+        verbs[Math.floor(Math.random() * verbs.length)] + ' ' +
+        determiners[Math.floor(Math.random() * determiners.length)] + ' ' +
+        adjectives[Math.floor(Math.random() * adjectives.length)] + ' ' +
+        nouns[Math.floor(Math.random() * nouns.length)] + '.';
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div id="pageContainer">
+      <v-app-bar
+          dark
+          color="blue">
+
+        <v-toolbar-title color="white">Vue</v-toolbar-title>
+      </v-app-bar>
+      <v-container
+          class="d-flex  justify-center my-10">
+        <v-card class="col-12 col-lg-4 mx-auto"
+                max-width="450">
+          <v-img
+              contain
+              class="align-self-center"
+              max-height="400"
+              :src="selectedImage"
+          ></v-img>
+<!--          <v-img>{{ message }}</v-img>-->
+          <v-card-actions class="justify-center ma-4">
+            <v-btn color="primary" outlined @click="randomizePicture">
+              Repeat
+              <v-icon
+                  right
+                  dark
+              >
+                mdi-replay
+              </v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </div>
+    <!--  </div>-->
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+const API_URL = "http://localhost:5000/quoteimgs";
+// import axios from "axios";
+
+// const context = require.context('./assets/init_img', true, /\.jpg$|.jpeg$|.png$/);
+// console.log('context', context);
+// console.log('keys', context.keys());
+// console.log('map keys', [...context.keys()].map(k => context(k)));
+// const imgPaths = [...context.keys()].map(k => context(k));
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      images: [],
+      // selectedImage:  require('@/assets/init_img/image.jpg'),
+      selectedImage: API_URL,
+    }
+  },
+  methods: {
+    randomizePicture() {
+      this.selectedImage = this.makeRandomImgURL();
+    },
+    makeRandomImgURL(){
+      return `${API_URL}?_=${~~(Math.random()*100000)}`;
+    }
+
   }
 }
+
 </script>
 
 <style>
-#app {
+#pageContainer {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
